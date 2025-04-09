37106c8d44f26ed9f54f84b99e575525c59a1879
Yizhuo Xu <z5460384@ad.unsw.edu.au>
2025-04-10 04:19:14 +1000

    update useWindowSizeStore

---
+++ airbnb-nextjs/src/store/useWindowSizeStore.ts
@@ -0,0 +1,28 @@
+import { WindowSizeTypes } from './../interfaces/models';
+import { create } from 'zustand';
+import { useEffect } from 'react';
+
+export const useWindowSizeStore = create<WindowSizeTypes>(() => ({
+  width: 0,
+  height: 0
+}));
+
+const updateWindowSize = () => {
+  useWindowSizeStore.setState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+};
+
+export function useWindowSizeEffect() {
+  useEffect(() => {
+    // 初始更新
+    updateWindowSize();
+    
+    // 监听窗口大小变化
+    window.addEventListener('resize', updateWindowSize);
+    
+    // 清理
+    return () => window.removeEventListener('resize', updateWindowSize);
+  }, []);
+}
